151961f34b9981961fbaec8c2cdb0a0b3a95596d
RAPHAEL PETERS <RKCPETERS@gmail.com>
2023-07-10 20:31:18 +0100

    Add skeleton code

---
+++ src/main/java/com/raphael/whatshouldicook/WhatshouldicookApplication.java
@@ -0,0 +1,13 @@
+package com.raphael.whatshouldicook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatshouldicookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatshouldicookApplication.class, args);
+	}
+
+}
