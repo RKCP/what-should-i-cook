934dcb6d7e299c486513c91656d4f89426b5efb4
RAPHAEL PETERS <RKCPETERS@gmail.com>
2023-07-31 21:24:11 +0100

    Add drop area to webpage temporarily

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,3 +1,52 @@
+var dropArea = document.getElementById('dropArea');
+var fileInput = document.getElementById('fileInput');
+var uploadForm = document.getElementById('uploadForm');
+
+// Prevent default behavior when files are dragged and dropped onto the drop area
+dropArea.addEventListener('dragenter', preventDefault, false);
+dropArea.addEventListener('dragleave', preventDefault, false);
+dropArea.addEventListener('dragover', preventDefault, false);
+dropArea.addEventListener('drop', handleDrop, false);
+
+// Open file dialog when the drop area is clicked
+dropArea.addEventListener('click', function() {
+    fileInput.click();
+});
+
+// Handle file selection
+fileInput.addEventListener('change', handleFileSelection);
+
+function preventDefault(e) {
+    e.preventDefault();
+    e.stopPropagation();
+}
+
+function handleDrop(e) {
+    preventDefault(e);
+
+    var files = e.dataTransfer.files;
+
+    // Process the dropped files or perform further actions
+    handleFiles(files);
+}
+
+function handleFileSelection(e) {
+    var files = e.target.files;
+
+    // Process the selected files or perform further actions
+    handleFiles(files);
+}
+
+function handleFiles(files) {
+    if (files.length > 0) {
+        var formData = new FormData();
+        formData.append('fileInput', files[0]);
+
+        // Submit the form with the selected file
+        uploadForm.submit(); // uploadForm refers to a DOM element with the id uploadForm
+    }
+}
+
 function changeOpacity(svgItem) {
   svgItem.style.opacity = 0.5; // Set the opacity to the desired value when the SVG item is clicked
   // You can also add additional logic here if needed
